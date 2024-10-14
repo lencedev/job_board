2d7a25db05aa53c34f11dbad08932126b0b22baa
lencedev <owen1.bolling@epitech.eu>
2024-10-14 21:36:55 +0200

    udpate

--- frontend/src/Component/JobsList.jsx
+++ frontend/src/Component/JobsList.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import "../Styles/Components/JobsList.scss";
 
-
 const JobsList = () => {
 
   const [blog, setBlogs] = useState([]);
@@ -17,22 +16,24 @@ const JobsList = () => {
       })
   }, [])
   return (
-    <div className='test'>
+    <div className='container'>
       <h1>Jobs List</h1>
       {
         blog.length === 0 ? <p>Chargement...</p> : null
       }
       <h2>{blog.length} Offres d'emploi</h2>
-      {
-        blog.map(job => {
-          return (
-            <div key={job._id}>
-              <h2>{job.title}</h2>
-              <p>{job.description}</p>
-            </div>
-          )
-        })
-      }
+      <div className="jobs-container">
+        {
+          blog.map(job => {
+            return (
+              <div key={job._id} className="job-card">
+                <h2>{job.title}</h2>
+                <p>{job.description}</p>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
